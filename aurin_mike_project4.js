875d589ac41e81fbc57f0e032b13cefbd8ca1325
Mike Aurin <mikeaurin@gmail.com>
2014-02-23 14:49:30 -0500

    Project 4 initial commit

---
+++ aurin_mike_project4.js
@@ -0,0 +1,29 @@
+// Mike Aurin
+// SDI 1402
+// Project 4
+// Building a Library of Functions
+
+// Question 1
+// Does a number follow the same pattern as 123-456-7890
+
+var myTelephone = "407-400-4321";
+var myOtherNumber = "123-456-7890";
+
+if (myOtherNumber.length === myTelephone.length) {
+	console.log("Question 1: " + true); 
+	} else {
+		console.log("Question 1: " + false)
+
+};
+	
+//Question 2
+// Does an email contain a ".edu" in the string
+
+
+myEmail = "mikeaurin@fullsail.edu";
+emailCharacters = myEmail.match(["a-z"]+ "@" + ["a-z",".","a-z"]);
+if (emailCharacters != myEmail) {
+    console.log("Question 2: " + myEmail + " is a valid email address.");
+	} else {
+		console.log("Question 2: " + "Please enter a valid email address.");
+};
