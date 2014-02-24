5e1d95a3fe321e2fdcec8934bd6ddbe3efd01ee9
Mike Aurin <mikeaurin@gmail.com>
2014-02-24 18:35:30 -0500

    Added the capitalization function for question 4

--- aurin_mike_project4.js
+++ aurin_mike_project4.js
@@ -27,3 +27,29 @@ if (emailCharacters != myEmail) {
 	} else {
 		console.log("Question 2: " + "Please enter a valid email address.");
 };
+
+// Question 3
+// Does a string begin with "http://" or "https://"
+
+var myWebsite = "http://www.droid-life.com";
+var myOtherWebsite = "https://www.phonearena.com";
+
+if (myWebsite.indexOf(0-6) !== "http://") {
+	console.log("Question 3: " + true)
+		} else { 
+			console.log("Question 3: " + false)
+}		if (myOtherWebsite.indexOf(0-7) !== "https://") {
+		console.log("Question 3: " + true)
+			} else {
+		console.log("Question 3: " + false)
+		// Nested Conditional: Deliverable 3
+};
+
+// Question 4
+//  Take a string that is all lowercase and uppercase the first letter of each word
+
+String.prototype.capitalize = function() {
+    return this.charAt(0,11).toUpperCase() + this.slice(1);
+}
+
+console.log("Question 4: " + "capitalize this!".capitalize());
